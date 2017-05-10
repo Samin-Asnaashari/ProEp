533546599365608193e636e511d72462e0d28f64
agneswad <agnesaba@live.com>
2017-05-10 14:18:35 +0200

    fixing person controller to student and teacher

---
+++ Fontys-ProEp/course-registration-management-system/src/main/java/org/fontys/course/registration/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package org.fontys.course.registration.repository;
+
+import org.fontys.course.registration.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+}
+
+
---
+++ Fontys-ProEp/course-registration-management-system/src/main/java/org/fontys/course/registration/service/StudentService.java
@@ -0,0 +1,37 @@
+package org.fontys.course.registration.service;
+
+import org.fontys.course.registration.model.Student;
+import org.fontys.course.registration.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+	public List<Student> GetAllStudents() {
+		return this.studentRepository.findAll();
+	}
+
+	public Student GetStudent(Integer pcn) {
+		return this.studentRepository.findOne(pcn);
+	}
+
+	public void AddStudent(Student student) {
+		this.studentRepository.save(student);
+	}
+
+	public void UpdateStudent(Student student) {
+		this.studentRepository.save(student);
+	}
+
+	public void DeleteStudent(Integer pcn) {
+		this.studentRepository.delete(pcn);
+	}
+
+
+}
