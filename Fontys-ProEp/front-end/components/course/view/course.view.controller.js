fc0ccf8e2735db0bac5ec45bbbd416ef19df7832
agneswad <agnesaba@live.com>
2017-06-10 08:21:31 +0200

    getting students for the registration states

--- Fontys-ProEp/front-end/components/course/view/course.view.controller.js
+++ Fontys-ProEp/front-end/components/course/view/course.view.controller.js
@@ -6,11 +6,17 @@ angular.module('appComponent.courseView').controller('courseViewCtrl', function 
     var vm = this;
     vm.course = courseResolve.course;
     vm.acceptedRegistrations = acceptedRegistrationsResolve.acceptedRegistrations;
+    console.log(vm.acceptedRegistrations, "accepted");
     vm.PendingRegistrations = pendingRegistrationsResolve.PendingRegistrations;
     vm.declineRegistrations = declineRegistrationsResolve.declineRegistrations;
+    vm.acceptedRegistrationStudents = [];
 
-    vm.provideCourse = function (course) {
-        console.log('course provided', course);
-
-    }
+    vm.acceptedStudents = function () {
+        angular.forEach(vm.acceptedRegistrations, function (s) {
+            var temp;
+            temp = studentService.getStudent(s.studentPcn);
+            vm.acceptedRegistrationStudents.push(temp);
+        });
+        return vm.acceptedRegistrationStudents;
+        };
 });
