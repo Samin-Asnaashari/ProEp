de06d339a894ad7800e0e18a1765d1bdddc2a480
Samin-Asnaashari <asn.samin@gmail.com>
2017-06-18 17:14:40 +0200

    edit

---
+++ Fontys-ProEp/front-end/components/config/app.ui-notification.js
@@ -0,0 +1,15 @@
+/**
+ * ui-notification configuration
+ * TODO: Where to put this?
+ */
+// angular.module('appComponent').config(function (NotificationProvider) {
+//     NotificationProvider.setOptions({
+//         delay: 2000,
+//         startTop: 20,
+//         startRight: 10,
+//         verticalSpacing: 20,
+//         horizontalSpacing: 20,
+//         positionX: 'right',
+//         positionY: 'top'
+//     });
+// });
